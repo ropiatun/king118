4ec248c1da0c7f1ba1499af15e7f261454d5dac4
agusriyadi <agusriyadi200@gmail.com>
2020-08-17 21:26:01 +0700

    add fitures "status aktive"

--- application/controllers/admin/Sales.php
+++ application/controllers/admin/Sales.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class Sales extends CI_Controller
 {
@@ -16,6 +16,36 @@ class Sales extends CI_Controller
         $data["sales"] = $this->sales_model->getAll();
         $this->load->view("admin/sales/list", $data);
     }
+    public function upadatestatus()
+    {
+        $where  = $this->input->post('id');
+        $user = $this->db->get_where('user', ['id_user' => $where])->row_array();
+        if ($user['aktif'] == 0) {
+            $aktif = 'Tidak Aktif';
+        } else if ($user['aktif'] == 1) {
+            $aktif = 'Aktif';
+        }
+        $data = array(
+            'user' => $user,
+            'aktif' => $aktif
+        );
+        echo json_encode($data);
+    }
+    public function upadatestatusbaru()
+    {
+        $id = $this->input->post('idistatus');
+        $status = $this->input->post('statusedit');
+        if ($status == 'Aktif') {
+            $statusconvert = 1;
+        } elseif ($status == 'Tidak Aktif') {
+            $statusconvert = 0;
+        }
+
+        $this->db->set('aktif', $statusconvert);
+        $this->db->where('id_user', $id);
+        $this->db->update('user');
+        redirect('admin/Sales');
+    }
 
     public function tambah()
     {
@@ -25,8 +55,8 @@ class Sales extends CI_Controller
 
         if ($validation->run()) {
             $sales->save();
-        // var_dump($sales->save());
-        // die;
+            // var_dump($sales->save());
+            // die;
             $this->session->set_flashdata('sukses', 'Berhasil disimpan');
             redirect('admin/sales');
         }
@@ -34,17 +64,12 @@ class Sales extends CI_Controller
 
         $this->load->view("admin/sales/tambah");
     }
-    public function aktifupdate()
-        {
-            $where  = $this->input->post('id');
-            $kategori = $this->db->get_where('tb_e_kategori', ['id_kategori' => $where])->row_array();
-            echo json_encode($kategori);
-        }
+
 
     public function edit($id = null)
     {
         if (!isset($id)) redirect('admin/sales');
-       
+
         $sales = $this->sales_model;
         $validation = $this->form_validation;
         $validation->set_rules($sales->rules());
@@ -56,25 +81,24 @@ class Sales extends CI_Controller
 
         $data["sales"] = $produk->getById($id);
         if (!$data["sales"]) show_404();
-        
+
         $this->load->view("admin/sales/edit", $data);
     }
 
-    function detail($id){
-         $data = [
-            'kingsamadenganraja' => $this->db->get_where('sales',['id_user'=>$id])->row_array(),
+    function detail($id)
+    {
+        $data = [
+            'kingsamadenganraja' => $this->db->get_where('sales', ['id_user' => $id])->row_array(),
         ];
         $this->load->view("admin/sales/detail", $data);
     }
 
-     public function delete($id=null)
+    public function delete($id = null)
     {
         if (!isset($id)) show_404();
-        
+
         if ($this->sales_model->delete($id)) {
             redirect(site_url('admin/sales'));
         }
     }
-
-    
 }
--- application/views/admin/sales/list.php
+++ application/views/admin/sales/list.php
@@ -39,42 +39,49 @@
 										<th>Foto KTP</th>
 										<th>Foto Selfie KTP</th>
 										<th>Foto Profil</th>
+										<th>Status</th>
 										<th>Aksi</th>
 									</tr>
 								</thead>
 								<tbody>
 									<?php
-										$no=1;
-										foreach ($sales as $sales): ?>
-									<tr>
-										<td><?php echo $no++ ?></td>
-										<td width="150">
-											<?php echo $sales->nama ?>
-										</td>
-										<td>
-											<?php echo  $sales->alamat ?>
-										</td>
-										<td>
-											<?php echo $sales->no_telepon ?>
-										</td>
-										<td>
-											<img src="<?php echo base_url('upload/sales/'.$sales->foto_ktp) ?>" width="64" />
-										</td>
-										<td>
-											<img src="<?php echo base_url('upload/sales/'.$sales->selfie_ktp) ?>" width="64" />
-										</td>
-										<td>
-											<img src="<?php echo base_url('upload/sales/'.$sales->foto_profil) ?>" width="64" />
-										</td>
-										<td>
-											<a href="<?php echo site_url('admin/sales/detail/'.$sales->id_user) ?>"><i class="fas fa-search-plus" style="color:green;"></i></a>&nbsp;&nbsp;&nbsp;
-										
-											<a onclick="deleteConfirm('<?php echo site_url('admin/sales/delete/'.$sales->id_user) ?>')"><i class="fa fa-trash" style="color: red;"></i></a>
-											<a href="<?php echo site_url('admin/sales/detail/'.$sales->id_user) ?>" data-toggle="modal" data-kode="<?= $sales->id_user ?>" data-target="#editkategori"><i class="fas fa-search-plus editkategori" style="color:green;"></i></a>&nbsp;&nbsp;&nbsp;
+									$no = 1;
+									foreach ($sales as $sales) : ?>
+										<tr>
+											<td><?php echo $no++ ?></td>
+											<td width="150">
+												<?php echo $sales->nama ?>
+											</td>
+											<td>
+												<?php echo  $sales->alamat ?>
+											</td>
+											<td>
+												<?php echo $sales->no_telepon ?>
+											</td>
+											<td>
+												<img src="<?php echo base_url('upload/sales/' . $sales->foto_ktp) ?>" width="64" />
+											</td>
+											<td>
+												<img src="<?php echo base_url('upload/sales/' . $sales->selfie_ktp) ?>" width="64" />
+											</td>
+											<td>
+												<img src="<?php echo base_url('upload/sales/' . $sales->foto_profil) ?>" width="64" />
+											</td>
+											<td>
+												<?php if ($sales->aktif == 0) { ?>
+													<a href="#" class="btn btn-danger updatestatus" data-kode="<?= $sales->id_user ?>" data-toggle="modal" data-target="#updatestatus">
+														Tidak Aktif
+													<?php } elseif ($sales->aktif == 1) { ?>
+														<a href="#" class="btn btn-success updatestatus" data-kode="<?= $sales->id_user ?>" data-toggle="modal" data-target="#updatestatus">
+															Aktif
+														<?php } ?>
+														</a></td>
+											<td>
+												<a href="<?php echo site_url('admin/sales/detail/' . $sales->id_user) ?>"><i class="fas fa-search-plus" style="color:green;"></i></a>&nbsp;&nbsp;&nbsp;
 
-										
-										</td>
-									</tr>
+												<a onclick="deleteConfirm('<?php echo site_url('admin/sales/delete/' . $sales->id_user) ?>')"><i class="fa fa-trash" style="color: red;"></i></a>
+											</td>
+										</tr>
 									<?php endforeach; ?>
 
 								</tbody>
@@ -96,31 +103,36 @@
 	<!-- /#wrapper -->
 	<!-- Modal -->
 
-<div class="modal fade" id="editkategori" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Edit Kategori</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form method="post" action="<?= base_url('admin/produk_admin/editkategori'); ?>">
-                    <input type="hidden" name="id_kategori_edit" id="id_kategori_edit" class="form-control">
-                    <div class="form-group">
-                        <label for="kategori_saat_ini">Kategori</label>
-                        <input type="text" name="nama_kategori_edit" id="nama_kategori_edit" class="form-control">
-                    </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger" data-dismiss="modal">Kembali</button>
-                <button type="submit" class="btn btn-primary" id="save_pesawat">Simpan</button>
-            </div>
-            </form>
-        </div>
-    </div>
-</div>
+	<div class="modal fade" id="updatestatus" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="staticBackdropLabel">Bukti Pemabayaran</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body" id="gambarshow">
+					<form method="post" action="<?= site_url('admin/Sales/upadatestatusbaru'); ?>">
+						<input type="hidden" name="idistatus" id="idistatus" class="form-control">
+						<div class="form-group">
+							<label for="kategori_saat_ini">status</label>
+							<select class="form-control" name="statusedit" id="statusedit">
+								<option selected="" disabled="">Pilih status</option>
+								<option value="Aktif">Aktif</option>
+								<option value="Tidak Aktif">Tidak Aktif</option>
+							</select>
+						</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-danger" data-dismiss="modal">Kembali</button>
+					<button type="submit" class="btn btn-primary" id="save_pesawat">Simpan</button>
+				</div>
+				</form>
+			</div>
+
+		</div>
+	</div>
 
 	<?php $this->load->view("admin/_partials/scrolltop.php") ?>
 	<?php $this->load->view("admin/_partials/modal.php") ?>
@@ -128,29 +140,30 @@
 	<?php $this->load->view("admin/_partials/js.php") ?>
 
 	<script>
-function deleteConfirm(url){
-	$('#btn-delete').attr('href', url);
-	$('#deleteModal').modal();
-}
-</script>
-<script type="text/javascript">
-    $(document).on('click', '.editkategori', function() {
-        $.ajax({
-            url: base_url + 'admin/Sales/aktifupdate',
-            method: 'POST',
-            data: {
-                id: $(this).data('kode'),
-            },
-            success: function(response) {
-                data = JSON.parse(response);
-                console.log(data);
-                $('#id_kategori_edit').val(data.id_kategori);
-                $('#nama_kategori_edit').val(data.nama_kategori);
-            }
-        });
-    });
-    
-</script>
+		function deleteConfirm(url) {
+			$('#btn-delete').attr('href', url);
+			$('#deleteModal').modal();
+		}
+	</script>
+
+	<script type="text/javascript">
+		var base_url = '<?= base_url() ?>'
+		$(document).on('click', '.updatestatus', function() {
+			$.ajax({
+				url: base_url + 'index.php/admin/Sales/upadatestatus',
+				method: 'POST',
+				data: {
+					id: $(this).data('kode'),
+				},
+				success: function(response) {
+					data = JSON.parse(response);
+					console.log(data);
+					$('#idistatus').val(data.user.id_user);
+					$('#statusedit').val(data.aktif);
+				}
+			});
+		});
+	</script>
 
 </body>
 
